90c14aa28f27aea0c84af06bd4d6342949f0d03e
snzyrn <snzyaren@gmail.com>
2024-08-09 16:16:25 +0300

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
             {LOGOS.map((logo, index) => (
               <div
                 className="slide flex w-[250px] 2xl:w-[350px] items-center justify-center px-5"
-                key={index}
+                key={`logo-${index}`} // Adding a unique key
               >
                 {logo}
               </div>
@@ -96,7 +96,7 @@ export default function Home() {
             {LOGOS.map((logo, index) => (
               <div
                 className="slide flex w-[250] 2xl:w-[350px] items-center justify-center px-5"
-                key={index + LOGOS.length}
+                key={`logo-${index + LOGOS.length}`} // Adding a unique key
               >
                 {logo}
               </div>
@@ -106,22 +106,22 @@ export default function Home() {
         <FadeUp className="mx-24 lg:mb-20 lg:mt-20 flex lg:flex-row flex-col justify-center items-center">
           <div className="lg:w-1/2 w-full lg:mr-12 2xl:w-5/12">
             <h2 className="text-text font-medium text-5xl mb-5 lg:mt-12 mt-5">
-              {t('homePage.whoWeAre.title')}
+              {t("homePage.whoWeAre.title")}
             </h2>
             <p className="text-gray-500 text-lg 2xl:w-2/3 2xl:text-xl">
-              {t('homePage.whoWeAre.description')}
+              {t("homePage.whoWeAre.description")}
             </p>
             <button
               type="button"
               onClick={() => (window.location.href = "/about")}
               className="mt-10 inline-block rounded bg-text px-6 pb-2 pt-2.5 2xl:mt-16 font-medium leading-normal text-neutral-50 shadow-dark-3 transition duration-150 ease-in-out hover:bg-neutral-700 hover:shadow-dark-2 focus:bg-neutral-700 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-dark-2 motion-reduce:transition-none"
             >
-              {t('homePage.whoWeAre.buttonText')}
+              {t("homePage.whoWeAre.buttonText")}
             </button>
           </div>
           <Image
             src={About}
-            alt={t('homePage.whoWeAre.altText')}
+            alt={t("homePage.whoWeAre.altText")}
             width={550}
             className="my-20 lg:my-0"
           />
@@ -130,23 +130,23 @@ export default function Home() {
         <FadeUp className="mx-24 flex lg:flex-row flex-col justify-center items-center">
           <Image
             src={Student}
-            alt={t('homePage.careerSupport.altText')}
+            alt={t("homePage.careerSupport.altText")}
             width={400}
             className="hidden lg:block mr-32"
           />
           <div className="lg:w-1/2 w-full lg:mr-12 2xl:w-5/12 my-24">
             <h2 className="text-text font-medium text-5xl mb-20 mt-12">
-              {t('homePage.careerSupport.title')}
+              {t("homePage.careerSupport.title")}
             </h2>
             <p className="text-gray-500 text-lg 2xl:text-xl">
-              {t('homePage.careerSupport.description')}
+              {t("homePage.careerSupport.description")}
             </p>
             <button
               type="button"
               onClick={() => (window.location.href = "/services")}
               className="mt-10 2xl:mt-16 inline-block rounded bg-text px-6 pb-2 pt-2.5 font-medium leading-normal text-neutral-50 shadow-dark-3 transition duration-150 ease-in-out hover:bg-neutral-700 hover:shadow-dark-2 focus:bg-neutral-700 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-dark-2 motion-reduce:transition-none"
             >
-              {t('homePage.careerSupport.buttonText')}
+              {t("homePage.careerSupport.buttonText")}
             </button>
           </div>
         </FadeUp>
@@ -155,68 +155,68 @@ export default function Home() {
           <div className="flex flex-col items-center justify-center bg-white rounded-xl lg:w-1/4 2xl:w-1/6 w-1/2 py-10 p-5">
             <Image
               src={Education}
-              alt={t('homePage.serviceCards.education.altText')}
+              alt={t("homePage.serviceCards.education.altText")}
               width={50}
               height={50}
               className="mb-3"
             />
             <h3 className="text-text text-xl font-medium 2xl:text-2xl">
-              {t('homePage.serviceCards.education.title')}
+              {t("homePage.serviceCards.education.title")}
             </h3>
             <p className="text-gray-500 text-center mt-2">
-              {t('homePage.serviceCards.education.description')}
+              {t("homePage.serviceCards.education.description")}
             </p>
           </div>
           <div className="flex flex-col items-center justify-center bg-white rounded-xl lg:w-1/4 2xl:w-1/6 w-1/2 py-10 p-5">
             <Image
               src={Book}
-              alt={t('homePage.serviceCards.germanLessons.altText')}
+              alt={t("homePage.serviceCards.germanLessons.altText")}
               width={50}
               height={50}
               className="mb-3"
             />
             <h3 className="text-text text-xl 2xl:text-2xl font-medium">
-              {t('homePage.serviceCards.germanLessons.title')}
+              {t("homePage.serviceCards.germanLessons.title")}
             </h3>
             <p className="text-gray-500 text-center mt-2">
-              {t('homePage.serviceCards.germanLessons.description')}
+              {t("homePage.serviceCards.germanLessons.description")}
             </p>
           </div>
           <div className="flex flex-col items-center justify-center bg-white rounded-xl lg:w-1/4 2xl:w-1/6 w-1/2 py-10 p-5">
             <Image
               src={Document}
-              alt={t('homePage.serviceCards.consulting.altText')}
+              alt={t("homePage.serviceCards.consulting.altText")}
               width={50}
               height={50}
               className="mb-3"
             />
             <h3 className="text-text text-xl 2xl:text-2xl font-medium">
-              {t('homePage.serviceCards.consulting.title')}
+              {t("homePage.serviceCards.consulting.title")}
             </h3>
             <p className="text-gray-500 text-center mt-2">
-              {t('homePage.serviceCards.consulting.description')}
+              {t("homePage.serviceCards.consulting.description")}
             </p>
           </div>
         </div>
         <FadeUp className="mx-24 lg:mt-20 flex lg:flex-row flex-col justify-center items-center">
           <div className="lg:w-1/2 w-full lg:mr-12 2xl:w-5/12 mt-12">
             <h2 className="text-text font-medium text-5xl mb-5 mt-12">
-              {t('homePage.exploreCourses.title')}
+              {t("homePage.exploreCourses.title")}
             </h2>
             <p className="text-gray-500 text-lg 2xl:text-xl">
-              {t('homePage.exploreCourses.description')}
+              {t("homePage.exploreCourses.description")}
             </p>
             <button
               type="button"
               onClick={() => (window.location.href = "/courses")}
               className="mt-10 2xl:mt-16 inline-block rounded bg-text px-6 pb-2 pt-2.5 font-medium leading-normal text-neutral-50 shadow-dark-3 transition duration-150 ease-in-out hover:bg-neutral-700 hover:shadow-dark-2 focus:bg-neutral-700 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-dark-2 motion-reduce:transition-none"
             >
-              {t('homePage.exploreCourses.buttonText')}
+              {t("homePage.exploreCourses.buttonText")}
             </button>
           </div>
           <Image
             src={Student2}
-            alt={t('homePage.exploreCourses.altText')}
+            alt={t("homePage.exploreCourses.altText")}
             width={550}
             className="my-12 lg:my-0"
           />
